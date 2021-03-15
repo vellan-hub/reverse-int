7cea0b44bbd92ae37c7f86507cc74e1019c5f3bd
Vitaly Yakubovski <falkon777.13@gmail.com>
2021-03-15 11:06:39 +0300

    feat: add function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse (number) {
+    if (number < 0 ) {number = number * -1};
+    return +number.toString().split("").reverse().join("")
 }
